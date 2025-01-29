50c7282bd049db5a289b7032280eef2094253455
Jeydo <jeydo@pop-os.fritz.box>
2025-01-29 13:29:18 +0100

    added blur filter

--- sketch.js
+++ sketch.js
@@ -1,3 +1,8 @@
+/**
+ * Kurs: Generative Gesltatung, Semesterabgabe 2025
+ * Author: Silas Hering, 901107
+ */
+
 class Data{
 	// ABSOLUTE ORIENTATION
 	absoluteOrientation = undefined;
@@ -24,24 +29,22 @@ class Data{
 	// DIMENSIONS
 	dimensions = undefined;
 
-	constructor(){
-		/*
-		this.#getAcceleration();
-		*/
-	}
+	// SALT
+	/** @type {Number|undefined} - Random value */
+	salt = undefined;
 
 	initSensors(){
-		//this.#initAbsoluteOrientation();
+		this.#initAbsoluteOrientation();
 		this.#initSound();
-		//this.#initAcceleration();
+		this.#initAcceleration();
 		this.#initAltitude();
-		this.#getDeviceDimensions();
+		this.#initDeviceDimensions();
+		this.#initSalt();
 	}
 
 	#initAbsoluteOrientation(){
 	}
 	#getAbsoluteOrientation(){
-		console.log("absolute orientation");
 	};
 
 	#initSound(){
@@ -62,7 +65,7 @@ class Data{
 	};
 
 	#initAcceleration(){
-	}
+	};
 	#getAcceleration(){
 	};
 
@@ -70,7 +73,7 @@ class Data{
 		if("geolocation" in navigator){
 			let watchListener = navigator.geolocation.watchPosition(
 				(position) => {
-					this.#current_altitude = position.coords.altitude; // Is null on non mobile devices
+					this.#current_altitude = position.coords.altitude; // Is null on non-mobile devices
 					this.#current_latitude = position.coords.latitude;
 					this.#current_longitude = position.coords.longitude;
 				},
@@ -82,34 +85,101 @@ class Data{
 			throw new Error("Geolocation is not supported");
 		}
 	};
-	#getAltitude(){
+	#getPositionData(){
 		this.altitude = this.#current_altitude;
 		this.latitude = this.#current_latitude;
 		this.longitude = this.#current_longitude;
 	};
 
-	#getDeviceDimensions(){
+	#initDeviceDimensions(){
 		this.dimensions = { width: window.innerWidth, height: window.innerHeight };
 	};
+
+	/**
+	 * Generates a random number that can be used to make results more different
+	 */
+	#initSalt(){
+		const SALT_MIN = 0;
+		const SALT_MAX = 5;
+		let randomNumber = Math.floor(Math.random() * (SALT_MAX - SALT_MIN + 1) + SALT_MIN);
+		this.salt = randomNumber;
+	}
 
 	/**
 	 * Refreshes all gathered data
 	 */
 	refresh(){
+		this.#getAbsoluteOrientation();
 		this.#getSound();
-		this.#getAltitude();
+		this.#getAcceleration();
+		this.#getPositionData();
 	}
+}
+
+let myShader;
+function preload(){
+	//myShader = loadShader("shaders/shader.vert", "shaders/shader.frag");
+	// shader: https://github.com/aferriss/p5jsShaderExamples/tree/gh-pages
 }
 
 let data;
 function setup() {
-	createCanvas(400, 400);
+	let canvas = createCanvas(400, 400, WEBGL);
+	//debugMode(GRID);
+	canvas.parent("canvas-container");
+
 	data = new Data();
 	data.initSensors();
+	noStroke();
+	// DARKEST is goog
+	//blendMode(DARKEST);
 }
 
+// wenn leise = kreise langsam drehend
+// wenn lauter = viele Stangen voreinander
+// drehbewegung an Lautstärke koppeln
+// farbe an Lautstärke koppeln
+
 function draw() {
+	//shader(myShader);
 	background(200);
+	orbitControl();
+
+	/*
 	data.refresh();
-	console.log(data.sound, data.dimensions, data.altitude, data.longitude, data.latitude);
+	console.log(data.sound, data.dimensions, data.altitude, data.longitude, data.latitude, data.salt);
+	*/
+
+	
+	/*
+	pointLight(255, 255, 255, -100, -100, -100); // red
+	pointLight(0, 0, 255, 100, 100, 100); // blue
+	pointLight(0, 255, 0, -80, 20, -50); // yellow
+
+	specularMaterial(255, 0, 0);
+	*/
+
+	pointLight(255, 255, 255, -100, -100, -100); // red
+	pointLight(0, 0, 255, 100, 100, 100); // blue
+	pointLight(0, 255, 0, -80, 20, -50); // yellow
+	//pointLight(155, 155, 155, 150, -60, -50); // red-top-right
+
+	//specularMaterial(255, 0, 0);
+
+	for (let i = 0; i < 6; i++) {
+		push();
+		let x = cos(TWO_PI * i / 6) * 100;
+		let y = sin(TWO_PI * i / 6) * 100;
+		translate(x, y, 0);
+		//normalMaterial(); // Vibrant color reflection
+		if(i < 2){
+			specularMaterial(255, 0, 0);
+		}else if(i < 4){
+			specularMaterial(120, 120, 0);
+		}else{
+			specularMaterial(20, 120, 0);
+		}
+		sphere(50, 50, 50);
+		pop();
+	}
 }
