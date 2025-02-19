26d946beaac65f6d136ff4e3ae32d93eaf9e1c0d
Jeydo <jeydo@pop-os.fritz.box>
2025-02-19 10:26:39 +0100

    disabled audio replay

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@
  * Author: Silas Hering, 901107
  */
 
-class Data{
+class Data {
 	// ABSOLUTE ORIENTATION
 	absoluteOrientation = undefined;
 
@@ -34,7 +34,7 @@ class Data{
 	/** @type {Number|undefined} - Random value */
 	salt = undefined;
 
-	initSensors(){
+	initSensors() {
 		this.#initAbsoluteOrientation();
 		this.#initSound();
 		this.#initAcceleration();
@@ -43,30 +43,24 @@ class Data{
 		this.#initSalt();
 	}
 
-	#initAbsoluteOrientation(){
-	}
-	#getAbsoluteOrientation(){
-	};
+	#initAbsoluteOrientation() {}
+	#getAbsoluteOrientation() {}
 
-	#initSound(){
-		try{
+	#initSound() {
+		try {
 			this.#sound_mic = new p5.AudioIn();
-			let delay = new p5.Delay(0.74, 0.1);
-
-			this.#sound_mic.disconnect();
-			this.#sound_mic.connect(delay);
-
 			this.#sound_mic.start();
-		}catch(e){
+			this.#sound_mic.disconnect();
+		} catch (e) {
 			throw new Error(e);
 		}
 	}
-	#getSound(){
+	#getSound() {
 		let currentLevel = this.#sound_mic.getLevel();
 
 		this.#sound_process.push(currentLevel);
 		const LENGTH_OF_AVERAGE = 80;
-		if(this.#sound_process.length >= LENGTH_OF_AVERAGE){
+		if (this.#sound_process.length >= LENGTH_OF_AVERAGE) {
 			this.#sound_process.shift();
 		}
 
@@ -75,15 +69,13 @@ class Data{
 		let roundedAverage = Math.round(average * 100) / 100;
 
 		this.sound = roundedAverage;
-	};
+	}
 
-	#initAcceleration(){
-	};
-	#getAcceleration(){
-	};
+	#initAcceleration() {}
+	#getAcceleration() {}
 
-	#initAltitude(){
-		if("geolocation" in navigator){
+	#initAltitude() {
+		if ("geolocation" in navigator) {
 			let watchListener = navigator.geolocation.watchPosition(
 				(position) => {
 					this.#current_altitude = position.coords.altitude; // Is null on non-mobile devices
@@ -94,24 +86,24 @@ class Data{
 					throw new Error(error);
 				}
 			);
-		}else{
+		} else {
 			throw new Error("Geolocation is not supported");
 		}
-	};
-	#getPositionData(){
+	}
+	#getPositionData() {
 		this.altitude = this.#current_altitude;
 		this.latitude = this.#current_latitude;
 		this.longitude = this.#current_longitude;
-	};
+	}
 
-	#initDeviceDimensions(){
+	#initDeviceDimensions() {
 		this.dimensions = { width: window.innerWidth, height: window.innerHeight };
-	};
+	}
 
 	/**
 	 * Generates a random number that can be used to make results more different
 	 */
-	#initSalt(){
+	#initSalt() {
 		const SALT_MIN = 0;
 		const SALT_MAX = 5;
 		let randomNumber = Math.floor(Math.random() * (SALT_MAX - SALT_MIN + 1) + SALT_MIN);
@@ -121,7 +113,7 @@ class Data{
 	/**
 	 * Refreshes all gathered data
 	 */
-	refresh(){
+	refresh() {
 		this.#getAbsoluteOrientation();
 		this.#getSound();
 		this.#getAcceleration();
@@ -129,8 +121,8 @@ class Data{
 	}
 }
 
-class Blur{
-	constructor(x = 0, y = 0, dimensions = 200, givenIntensity = 10){
+class Blur {
+	constructor(x = 0, y = 0, dimensions = 200, givenIntensity = 10) {
 		this.initXPos = x;
 		this.xPos = x;
 
@@ -148,23 +140,23 @@ class Blur{
 
 		document.querySelector("#canvas-container").insertBefore(this.elem, this.elem.nextSilbling);
 
-		this.elem.style.left = this.xPos - (this.dimensions / 2) + "px";
-		this.elem.style.top = this.yPos - (this.dimensions / 2) + "px";
+		this.elem.style.left = this.xPos - this.dimensions / 2 + "px";
+		this.elem.style.top = this.yPos - this.dimensions / 2 + "px";
 		this.elem.style.width = this.dimensions + "px";
 		this.elem.style.height = this.dimensions + "px";
 		this.elem.style.backdropFilter = `blur(${this.intensity}px)`;
 	}
-	x(x){
+	x(x) {
 		this.xPos = x;
-		this.elem.style.left = this.xPos - (this.dimensions / 2) + "px";
+		this.elem.style.left = this.xPos - this.dimensions / 2 + "px";
 		return this;
 	}
-	y(y){
+	y(y) {
 		this.yPos = y;
-		this.elem.style.top = this.yPos - (this.dimensions / 2) + "px";
+		this.elem.style.top = this.yPos - this.dimensions / 2 + "px";
 		return this;
 	}
-	setIntensity(intens){
+	setIntensity(intens) {
 		this.intensity = intens;
 		this.elem.style.backdropFilter = `blur(${this.intensity}px)`;
 		return this;
@@ -194,11 +186,14 @@ function setup() {
 	blurs.push(new Blur((width / 6) * 3.5, (height / 6) * 3, (width / 6) * 2, 3));
 	blurs.push(new Blur((width / 6) * 2, (height / 6) * 4.5, (width / 6) * 4, 3));
 
-	for(let i = 0; i < 7; i++){
+	const AMOUNT_OF_SPHERES = 7;
+	const AMOUNT_OF_CYLINDERS = 7;
+
+	for (let i = 0; i < AMOUNT_OF_SPHERES; i++) {
 		let gradientArray = gradientColorArrayGenerator(random(2, 5));
 		sceneSpheres.push(createGradientTexture(100, 100, gradientArray));
 	}
-	for(let i = 0; i < 7; i++){
+	for (let i = 0; i < AMOUNT_OF_CYLINDERS; i++) {
 		let gradientArray = gradientColorArrayGenerator(random(4, 10));
 		sceneCylinders.push(createGradientTexture(100, 100, gradientArray));
 	}
@@ -209,8 +204,16 @@ function setup() {
  *
  * @param {Number} steps - steps the gradient will have
  */
-function gradientColorArrayGenerator(steps){
-	const AVAILABLE_COLORS = [color("#e5b750"), color("#d4632a"), color("#d76fc8"), color("#373bbe"), color("#5b2814"), color("#95bb5e"), color("#2d1c3f")];
+function gradientColorArrayGenerator(steps) {
+	const AVAILABLE_COLORS = [
+		color("#e5b750"),
+		color("#d4632a"),
+		color("#d76fc8"),
+		color("#373bbe"),
+		color("#5b2814"),
+		color("#95bb5e"),
+		color("#2d1c3f"),
+	];
 
 	// cap steps size
 	steps > AVAILABLE_COLORS.length ? (steps = AVAILABLE_COLORS.length) : (steps = steps);
@@ -218,7 +221,7 @@ function gradientColorArrayGenerator(steps){
 
 	// Fisher-Yates Shuffle
 	let cIndex = AVAILABLE_COLORS.length;
-	while (cIndex != 0){
+	while (cIndex != 0) {
 		let randomIndex = Math.floor(Math.random() * cIndex);
 		cIndex--;
 		[AVAILABLE_COLORS[cIndex], AVAILABLE_COLORS[randomIndex]] = [AVAILABLE_COLORS[randomIndex], AVAILABLE_COLORS[cIndex]];
@@ -237,16 +240,16 @@ function gradientColorArrayGenerator(steps){
  * @param {Any[]} colors - array of colors the gradient will get build from
  * @returns {Any} - Texture to be used on a 3d object
  */
-function createGradientTexture(w = 100, h = 100, colors){
+function createGradientTexture(w = 100, h = 100, colors) {
 	let cT = createGraphics(w, h);
 
 	// Loop over every pixel of the texture
-	for(let x = 0; x < w; x++){
-		for(let y = 0; y < h; y++){
+	for (let x = 0; x < w; x++) {
+		for (let y = 0; y < h; y++) {
 			let stopSize = Math.round(w / (colors.length - 1));
 			let colorEntryIndex = Math.floor(y / stopSize);
 
-			if(colorEntryIndex >= colors.length - 1){
+			if (colorEntryIndex >= colors.length - 1) {
 				colorEntryIndex = colors.length - 2;
 			}
 
@@ -254,7 +257,7 @@ function createGradientTexture(w = 100, h = 100, colors){
 			let nex = colors[colorEntryIndex + 1];
 			let inter = map(y % stopSize, 0, stopSize, 0, 1);
 			let lerpedColor = lerpColor(cur, nex, inter);
-			
+
 			let gradientStops = colors.length - 1;
 
 			cT.set(x, y, lerpedColor);
@@ -312,50 +315,54 @@ let allowChange = false;
 let changeType;
 
 function draw() {
-	background("#d9d6d3");
-	//orbitControl();
+	const BACKGROUND_COLOR = "#d9d6d3";
+	background(BACKGROUND_COLOR);
+	document.querySelector("body").style.backgroundColor = BACKGROUND_COLOR;
 
 	// Rotate blurs around their position
-	blurs.forEach(el => {
+	blurs.forEach((el) => {
 		el.x(el.initXPos + 50 * cos(blurRotation)).y(el.initYPos + 50 * sin(blurRotation));
-	})
+	});
 
 	data.refresh();
 	//console.log(data.sound, data.dimensions, data.altitude, data.longitude, data.latitude, data.salt);
-		
+
 	/** @type {Number} - Radius of animated object based on amount of objects in scene */
 	let R_Object = objectScaling / sin(PI / sceneSpheres.length);
 
-	if(data.sound >= 0.7){
+	if (data.sound >= 0.7) {
 		mode = "fast";
-	}else{
+	} else {
 		mode = "slow";
 	}
 
-	if(mode !== lastMode){
-		if(allowChange === false){
+	if (mode !== lastMode) {
+		if (allowChange === false) {
+			changeType = lastMode === "slow" ? "slow" : "fast";
+			/*
 			if(lastMode === "slow"){
 				changeType = "slow";
 			}else if(lastMode === "fast"){
 				changeType = "fast";
 			}
+			*/
 		}
 		allowChange = true;
 	}
 
-	if(allowChange){
-		if(objectScaling > smallestAnimationState && isSmallest === false){
+	if (allowChange) {
+		if (objectScaling > smallestAnimationState && isSmallest === false) {
 			objectScaling -= animationSpeed;
-		}else{
+		} else {
 			// fires when scaling animation is the smallest and the shown
 			// objects (spheres or cylinders) shall be switched
 			isSmallest = true;
 			showSpheresToggle = changeType === "slow" ? false : true;
 		}
 
-		if(isSmallest && objectScaling < biggestAnimationState){
+		if (isSmallest && objectScaling < biggestAnimationState) {
 			objectScaling += animationSpeed;
-		}else if(isSmallest && objectScaling >= biggestAnimationState){
+		} else if (isSmallest && objectScaling >= biggestAnimationState) {
 			// fires when the whole animation cycle is done
 			allowChange = false;
 			isSmallest = false;
@@ -363,10 +370,9 @@ function draw() {
 	}
 	lastMode = mode;
 
-
-	if(showSpheresToggle){
-		sceneSpheres.forEach((obj, i)=> {
-			let angleSphere = TWO_PI * i / sceneSpheres.length + objectRotation;
+	if (showSpheresToggle) {
+		sceneSpheres.forEach((obj, i) => {
+			let angleSphere = (TWO_PI * i) / sceneSpheres.length + objectRotation;
 			let x = R_Object * cos(angleSphere);
 			let y = R_Object * sin(angleSphere);
 
@@ -377,20 +383,20 @@ function draw() {
 			sphere(objectScaling, OBJECT_QUALITY, OBJECT_QUALITY);
 			pop();
 		});
-	}else{
-		sceneCylinders.forEach((obj, i)=> {
-			let angleCylinder = TWO_PI * i / (sceneCylinders.length * 2) + objectRotation;
+	} else {
+		sceneCylinders.forEach((obj, i) => {
+			let angleCylinder = (TWO_PI * i) / (sceneCylinders.length * 2) + objectRotation;
 
 			push();
 			translate(0, 0, i * -(cylinderRadius * 2));
 			rotateZ(angleCylinder * (i + 1));
 			texture(obj);
 			// Match length of cylinder to outer most part of spheres
-			cylinder(cylinderRadius, (R_Object * 2) + (objectScaling * 2), OBJECT_QUALITY, OBJECT_QUALITY);
+			cylinder(cylinderRadius, R_Object * 2 + objectScaling * 2, OBJECT_QUALITY, OBJECT_QUALITY);
 			pop();
 		});
 	}
 
 	blurRotation += rotationBlurSpeed;
-	objectRotation += rotationSpeed + (data.sound / 45);
+	objectRotation += rotationSpeed + data.sound / 45;
 }
